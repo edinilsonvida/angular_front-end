d37ca620fb5fb6c93e33f599984ecca26dd109cd
Edinilson Vida <edinilson.vida@hotmail.com>
2020-10-17 09:44:14 -0300

    Criação do componente PostDetailsComponente
    Criação dos métodos getPost e updatePost do componente PostDetailsComponente.

--- angularfront/src/app/components/post-details/post-details.component.ts
+++ angularfront/src/app/components/post-details/post-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-details.component.css']
 })
 export class PostDetailsComponent implements OnInit {
+  currentPost = null;
+  message = '';
 
-  constructor() { }
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.message = "";
+    this.getPost(this.route.snapshot.paramMap.get('id'));
   }
+
+  getPost(id): void {
+    this.postService.get(id)
+    .subscribe(
+      data => {
+        this.currentPost = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+    }
+
+    updatePost(): void {
+      this.postService.update(this.currentPost.id, this.currentPost)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'Esta postagem foi atualizada com sucesso.';
+        },
+        error => {
+          console.log(error);
+        });
+    }
+
+  
+
 
 }
